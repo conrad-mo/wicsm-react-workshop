80eaddf3fe1c2a3eb51d0dd60fca78d5220d176e
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2023-10-22 11:44:03 -0400

    Initial commit

---
+++ src/components/AddTodo.js
@@ -0,0 +1,14 @@
+import { Button, HStack, Input, useToast } from '@chakra-ui/react';
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+
+function AddTodo() { //Add input parameters.
+  // Your code here.
+  return (
+    // Your code here.
+    // Remove the div tag before you start coding.
+    <div></div>
+  );
+}
+
+export default AddTodo;
---
+++ src/components/TodoList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  HStack,
+  VStack,
+  Text,
+  IconButton,
+  StackDivider,
+  Spacer,
+  Badge,
+} from '@chakra-ui/react';
+import { FaTrash } from 'react-icons/fa';
+
+function TodoList() { //Add input parameters.
+  // Your code here.
+  return (
+    // Your code here.
+    // Remove the div tag before you start coding.
+    <div></div>
+  );
+}
+
+export default TodoList;
